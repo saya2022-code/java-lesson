625218e99db0683795e987c09c53e42acddd3621
saya2022-code <s.nishizaka@g.dreamcareer.co.jp>
2022-05-23 16:56:14 +0900

    5. 走行距離の出力

--- Bicycle.java
+++ Bicycle.java
@@ -1,15 +1,40 @@
 class Bicycle{
-    //アクセス権 データ型 プロパティ名
+    //①フィールドを定義
+    //アクセス権 データ型 フィールド名
      private String name;
+     private String color;
+     private int distance = 0; //ここでセットしているので、③の引数に追加しなくて良い
 
-    //インスタンスの生成時にフィールドに値をセットできるよう、コンストラクタを用意
-    Bicycle(String name){
+    //④インスタンスの生成時にフィールドに値をセットできるよう、コンストラクタを用意
+    Bicycle(String name,String color){
         this.name = name;
+        this.color = color;
     }
 
-    //Mainクラスからnameフィールドの値を取得するためにゲッターを定義
-    public String getName(){
-        return this.name;
+    //⑤Mainクラスからnameフィールドの値を取得するためにゲッターを定義
+     //⑦により、このメソッドは不要になる
+        // public String getName(){
+        //     return this.name;
+        // }
+
+    //⑦printDataメソッドを作り、出力の処理をまとめる
+    public void printData(){
+        System.out.println("名前：" + this.name);
+        System.out.println("色：" + this.color);
+        System.out.println("走行距離：" + this.distance + "km"); //初期状態、⑨で任意の数値を入れる
+    }
+
+    //⑨runメソッドを作り、走行した距離を表す
+     //引数に走行距離(任意の数値)を受け取る
+    public void printData(int distance){
+        //⑨-1 引数の値を出力
+        System.out.println(distance + "km走ります");
+
+        //⑨-2 引数の値をdistanceフィールドに追加
+        this.distance += distance;
+
+        //⑨-3 引数の値を足した距離を出力
+        System.out.println("走行距離：" + this.distance + "km");
     }
 
 }
--- Main.java
+++ Main.java
@@ -1,11 +1,23 @@
 class Main{
-  //mainクラスの定義
-  public static void main(String[] args) {
-      //Bicycleクラスのインスタンスを生成
-      Bicycle bicycle = new Bicycle("ビアンキ");
 
+  //②mainクラスの定義
+  public static void main(String[] args) {
+    //③Bicycleクラスのインスタンスを生成し、引数に値をセットする
+      Bicycle bicycle = new Bicycle("ビアンキ","緑");
+
+    //⑥ ⑤のgetName()を呼び出し、nameフィールドを出力
       System.out.println("【自転車の情報】");
-      System.out.println("名前：" + bicycle.getName());
+
+      //⑦で出力をまとめたので不要になる
+      // System.out.println("名前：" + bicycle.getName());
+
+    //⑧　⑦で出力をまとめたメソッドを呼び出す
+      bicycle.printData();
+
+    //⑩ ⑨のrunメソッドの引数に任意の数値を入れ、呼び出す
+      System.out.println("-----------------");  
+      bicycle.run(10);
+
 
     }
 }
